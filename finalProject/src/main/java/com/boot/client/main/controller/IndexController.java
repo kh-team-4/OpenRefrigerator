5897067958218a52dce08459278406849ceb6994
olhxxnz <110451636+hyoozo@users.noreply.github.com>
2023-03-15 13:56:36 +0900

    controller 추

--- finalProject/src/main/java/com/boot/client/main/controller/IndexController.java
+++ finalProject/src/main/java/com/boot/client/main/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.boot.client.main.comtroller;
+package com.boot.client.main.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
